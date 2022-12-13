607086b583cf2cf57f407d4a3315e5cf033d410e
edavud <enisdavud@gmail.com>
2022-12-13 15:04:03 -0500

    Initial commit

---
+++ forever_31_finalproject/src/main/java/com/edavud/forever_31_finalproject/Forever31FinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.edavud.forever_31_finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forever31FinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Forever31FinalprojectApplication.class, args);
+	}
+
+}
---
+++ forever_31_finalproject/src/test/java/com/edavud/forever_31_finalproject/Forever31FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edavud.forever_31_finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forever31FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
